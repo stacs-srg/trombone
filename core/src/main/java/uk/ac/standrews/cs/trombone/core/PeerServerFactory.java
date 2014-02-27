db77f7b5bee6f267594f1b81ddd7474d8395f230
Masih Hajiarab Derkani <m@derkani.org>
2014-02-27 14:28:21 +0000

    made thread usage in RPC more efficient

--- core/src/main/java/uk/ac/standrews/cs/trombone/core/PeerServerFactory.java
+++ core/src/main/java/uk/ac/standrews/cs/trombone/core/PeerServerFactory.java
@@ -22,7 +22,7 @@ public class PeerServerFactory extends ServerFactory<PeerRemote> {
     static {
 
         final NioEventLoopGroup parent_event_loop = new NioEventLoopGroup(0, new NamedThreadFactory("server_parent_event_loop_"));
-        final NioEventLoopGroup child_event_loop = new NioEventLoopGroup(500, new NamedThreadFactory("server_child_event_loop_"));
+        final NioEventLoopGroup child_event_loop = new NioEventLoopGroup(0, new NamedThreadFactory("server_child_event_loop_"));
         SERVER_BOOTSTRAP.group(parent_event_loop, child_event_loop);
         SERVER_BOOTSTRAP.channel(NioServerSocketChannel.class);
         SERVER_BOOTSTRAP.option(ChannelOption.TCP_NODELAY, true);
