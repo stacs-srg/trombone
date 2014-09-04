59bcb1dfa66f1297fc53dd47f7bec15bc1c9810c
Masih Hajiarab Derkani <m@derkani.org>
2014-09-04 14:09:41 +0100

    investigating serialization prblem on blub

--- evaluation/src/test/java/uk/ac/standrews/cs/trombone/evaluation/util/ScenarioUtilsTest.java
+++ evaluation/src/test/java/uk/ac/standrews/cs/trombone/evaluation/util/ScenarioUtilsTest.java
@@ -1,13 +1,13 @@
 package uk.ac.standrews.cs.trombone.evaluation.util;
 
 import org.junit.Test;
-import uk.ac.standrews.cs.trombone.evaluation.scenarios.StrongStabilization;
+import uk.ac.standrews.cs.trombone.evaluation.scenarios.Batch1EffectOfChurn;
 
 public class ScenarioUtilsTest {
 
     @Test
     public void testSaveScenarioAsJson() throws Exception {
 
-        System.out.println(ScenarioUtils.OBJECT_MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(new StrongStabilization()));
+        System.out.println(ScenarioUtils.OBJECT_MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(Batch1EffectOfChurn.getInstance().get().get(3)));
     }
 }
